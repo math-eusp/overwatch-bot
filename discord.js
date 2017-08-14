201ad977e7ff72e140cb45385ad9cb1657365e21
Matheus P <it.matheusp@gmail.com>
2017-08-14 18:29:04 -0300

    express fix

--- discord.js
+++ discord.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const owjs = require('overwatch-js');
 var express = require('express');
 var app = express();
-var port = process.env.PORT ;
+var port = process.env.PORT || 8080;
 
 
 app.get('/*', function(req, res){
